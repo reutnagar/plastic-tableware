b071cd0e51659b2e809e04a1324810e65a25a78e
sararo <Sarier@1993>
2017-01-10 12:47:35 +0200

    autoincrement

--- app/models/Item.js
+++ app/models/Item.js
@@ -1,4 +1,4 @@
-var mongoose = require('mongoose');
+/*var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Color = new Schema({
@@ -18,4 +18,41 @@ module.exports = mongoose.model('Item',{
 	quantity: Number,
 	minQuantity: Number,
 	colors:   [Color]    
+});*/
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    autoIncrement = require('mongoose-auto-increment');
+ 
+var connection = mongoose.createConnection("mongodb://test:1234qwer@ds054619.mlab.com:54619/plastic-tableware");
+ 
+autoIncrement.initialize(connection);
+ 
+var itemSchema = new Schema({
+    category: String,
+	subCategory: String,
+	index:{ type: Number, ref: 'index' },
+	name: String,
+	description: String,
+	location: String,
+	quantity: Number,
+	minQuantity: Number,
+	//colors:   [Color]   
+});
+
+var indexSchema = new mongoose.Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Item',itemSchema);
+//module.exports = mongoose.model('index',indexSchema);
+ 
+/*itemSchema.plugin(autoIncrement.plugin, 'Item');
+var Item = connection.model('Item', itemSchema);
+indexSchema.plugin(autoIncrement.plugin, 'index');*/
+itemSchema.plugin(autoIncrement.plugin, {
+    model: 'Item',
+    field: 'itemId',
+    startAt: 100,
+    incrementBy: 100
 });
