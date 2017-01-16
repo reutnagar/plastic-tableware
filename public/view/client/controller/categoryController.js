73dfb30751a3a08564c0c3f00bcac82116961f6d
sararo <sarierotenberg@gmail.com>
2017-01-16 11:36:06 +0200

    add client and server controllers pages in client and do functions: showAllCategories and showAllSubCategories

---
+++ public/view/client/controller/categoryController.js
@@ -0,0 +1,46 @@
+app.controller('itemCtrl',function($scope, $http) {
+	
+	
+  $scope.master = {}; 
+	
+	$scope.showAllCategories=function(){
+    $http.get('/itemCtrlServer/showAllCategories')
+                    .success(function(data){
+                        $scope.categories = data;
+                        console.log("Succeed loading");
+                    })
+                    .error(function(data){
+                        console.log("Error: "+data);
+					});
+	}
+	
+}
+
+	$scope.showAllSubCategory=function(category){
+	// use $.param jQuery function to serialize data from JSON 
+            var data = category;
+        
+            var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+
+            $http.post('/itemCtrlServer/showAllSubCategory', data, config)
+            .success(function (data, status, headers, config) {
+                $scope.PostDataResponse = data;
+                console.log("Succeed post showAllSubCategory");
+                $scope.subCategories.push(data);
+            })
+            .error(function (data, status, header, config) {
+                console.log("Error: "+data);
+                $scope.ResponseDetails = "Data: " + data +
+                    "<hr />status: " + status +
+                    "<hr />headers: " + header +
+                    "<hr />config: " + config;
+            });
+
+     console.log("showAllSubCategory function"+ category);
+     $scope.master = angular.copy(subCategory);
+
+};
