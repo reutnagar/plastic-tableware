955a47200753821e3ac5d5dc340f60ff5cf3f363
sararo <sarierotenberg@gmail.com>
2017-01-24 13:31:59 +0200

    add add func checkIfItemExistsInStock to router and orgenized the routes

--- app/routes.js
+++ app/routes.js
@@ -12,28 +12,35 @@ var path = require('path');
 router.use(express.static(path.join(__dirname , '../public/view/client')));
 router.use(express.static(path.join(__dirname , '../public/view/admin')));
 
-router.get('/admin/showAllItems', itemsController.showAllItems);
-router.get('/admin/showAllOrders', ordersController.showAllOrders);
-router.get('/admin/showLastOrders', ordersController.showLastOrders);
 router.get('/showAllCategories', categoryController.showAllCategories);
 router.get('/admin', function(req,res){
     res.sendFile(path.join(__dirname , '../public/view/admin/index.html'));
 });
 
+//funcs in itemsController
+router.get('/admin/showAllItems', itemsController.showAllItems);
 router.post('/admin/addItem', itemsController.addItem);
 router.post('/admin/deleteItem', itemsController.deleteItem);
 router.post('/admin/countItem', itemsController.countItem);
 router.post('/admin/changeItem', itemsController.changeItem);
 router.post('/getProductDetails', itemsController.getProductDetails);
 router.post('/getProductsOfSubCategory',itemsController.getProductsOfSubCategory);
+router.post('/checkIfItemExistsInStock',itemsController.checkIfItemExistsInStock);
+
+//funcs in ordersController
+router.get('/admin/showAllOrders', ordersController.showAllOrders);
+router.get('/admin/showLastOrders', ordersController.showLastOrders);
 router.post('/admin/ordersOfUserName',ordersController.ordersOfUserName);
 
+//funcs in categoryController
+router.post('/showAllSubCategory', categoryController.showAllSubCategories);
+
+//funcs in userController
 router.post('/admin/signIn',userController.signIn);
 router.post('/admin/signOut',userController.signOut);
 router.post('/admin/getSessionInfo',userController.getSessionInfo);
 
-router.post('/showAllSubCategory', categoryController.showAllSubCategories);
-
+//funcs in paymentController
 router.post('/processPayment', paymentController.processPayment);
 
 // export router
