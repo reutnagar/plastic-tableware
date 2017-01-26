a0a63e8e556c9aa60388d8892862a979eb0a0cbe
sararo <sarierotenberg@gmail.com>
2017-01-26 10:25:26 +0200

    create new schema called Quantity

---
+++ app/models/Quantity.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var QuantitySchema = new Schema({
+  name:   String,
+  image:  Buffer,
+  quantity: Number,
+  minQuantity: Number
+});
+module.exports = mongoose.model('Quantity', QuantitySchema);
