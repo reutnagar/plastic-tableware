164827c751d6b344d57fed6817c2859cce1ef935
ruty369 <ruty369@gmail.com>
2017-01-26 12:15:20 +0200

    add a notification directive
    alert on the dashboard when an item almost  reduce in stock

--- public/view/admin/controller/notificationCtrl.js
+++ public/view/admin/controller/notificationCtrl.js
@@ -1,5 +1,20 @@
 app.controller('notificationCtrl', function($scope, $http) {  
-   console.log("notification controller````````````````"); 
+$scope.type="הוזל מן המלאי"; 
+$scope.reduce=true;;  
+console.log("notification controller````````````````"); 
+   $scope.checkQuantity = function() {
+	 console.log("checkQuantity");
+	$http.get('/admin/checkQuantity')
+					.success(function(data){
+						$scope.quantity = data;
+						data="";
+						if(data=="")$scope.reduce=false;
+						console.log("Succeed loading");
+					})
+					.error(function(data){
+						console.log("Error: "+data);
+		 });
+}
 
 
 });
