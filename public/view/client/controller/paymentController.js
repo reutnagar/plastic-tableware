54e0e4dbb3147c4e94d6eff5279564956695b172
reutnagar <salreut1@gmail.com>
2017-01-28 21:00:34 +0200

    payment with angular

--- public/view/client/controller/paymentController.js
+++ public/view/client/controller/paymentController.js
@@ -1,26 +1,31 @@
-app.controller('listProductCtrl', function ($scope, $http) {
-    var handler = StripeCheckout.configure({
-        key: 'pk_test_BhOjkKLIAI0WnFdiStLTRHG2',
-        image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-        locale: 'auto',
-        token: function (token) {
-            // You can access the token ID with `token.id`.
-            // Get the token ID to your server-side code for use.
+//add paymeny dependency
+angular.module("clientApp").requires.push('angularPayments');
+
+//set publish key
+app.config(function ($window) {
+    $window.Stripe.setPublishableKey('pk_test_BhOjkKLIAI0WnFdiStLTRHG2');
+});
+app.controller('paymentCtl', function ($scope, $http, $location) {
+
+    // Stripe Response Handler
+    $scope.stripeCallback = function (code, result) {
+        if (result.error) {
+            window.alert('it failed! error: ' + result.error.message);
+        } else {
+            window.alert('success! token: ' + result.id);
+            var $payInfo = {
+                'token': result.id,
+                'customer_id': 111,
+                'total': 50
+            };
+
+            $http.post('/api/payreservation', $payInfo).success(function (data) {
+                if (data.status == "OK") {
+                    console.log('payment recieved!');
+                } else {
+                    console.log('ERROR: payment is not recieved!');
+                }
+            });
         }
-    });
-
-    document.getElementById('customButton').addEventListener('click', function (e) {
-        // Open Checkout with further options:
-        handler.open({
-            name: 'Plastic Tableware',
-            description: '2 widgets',
-            amount: 2000
-        });
-        e.preventDefault();
-    });
-
-    // Close Checkout on page navigation:
-    window.addEventListener('popstate', function () {
-        handler.close();
-    });
+    };
 });
