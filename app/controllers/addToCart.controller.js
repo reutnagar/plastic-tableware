3762a63b51ba1583670042dd94f3cbbd332302e4
sararo <sarierotenberg@gmail.com>
2017-01-30 00:11:32 +0200

    add func quantity in server and fix the makeAnOrder func

--- app/controllers/addToCart.controller.js
+++ app/controllers/addToCart.controller.js
@@ -34,23 +34,65 @@ function makeAnOrder(req,res) {
 	console.log("in makeAnOrder");
 	// Retrieve the object from storage
 	var retrievedObject = localStorage.getItem('addToCart');
-	addToCart = JSON.parse(retrievedObject);
+	myList = JSON.parse(retrievedObject);
 	
-	_userName = addToCart.userName;
-	_email = addToCart.email;
-	_address = addToCart.address;
-	_numItems = addToCart.numItems;
-	_payment : addToCart.payment;
+	item_id = myList._id;
+	color = myList.color;
+	sum = myList.sum;
 	
-	_item_id = addToCart._id;
-	_color = addToCart.color;
-	_sum = addToCart.sum;
+	console.log("get post request in server side");  
+    var body = '';
+        req.on('data', function (data) {
+            body += data;
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6) { 
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                req.connection.destroy();
+            }
+        });   
+        req.on('end', function () {
+        var POST = qs.parse(body); 
+		Item.findByIdAndUpdate(POST._id, {quantities.quantity : quantiy-POST.sum}.where(quantities.quantity ).gt(POST.sum).exec(function(err, doc) {
+			if(err){
+				throw err;
+			}
+			console.log("this is the doc variable: "+doc);
+			console.log("updated");
+		});
+		var newOrder = new Order {{userName:POST.userName,email:POST.email,address:POST.address,status:"התקבלה הזמנה",numItems:POST.numItems,payment:POST.payment});
+		var newOrderedItem = new OrderedItem({item_id:item_id,color:color,sum:sum});
+		newOrderedItem.save(function(err, newOrderedItem){
+		newOrder.quantities.push(newOrderedItem);
+		newOrder.save();
+		}):
 	
-	var newOrder = new Order {{userName:_userName,email: _email,address:_address,status:"התקבלה הזמנה",numItems:_numItems,payment:_payment});
-	var newOrderedItem = new OrderedItem({item_id:_item_id,color:_color,sum:_sum});
-	newOrderedItem.save(function(err, newOrderedItem){
-	newOrder.quantities.push(newOrderedItem);
-	newOrder.save();
-	}):
+}
+
+function quantity(req,res) {
+	console.log("in makeAnOrder");
+	console.log("get post request in server side");  
+    var body = '';
+        req.on('data', function (data) {
+            body += data;
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6) { 
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                req.connection.destroy();
+            }
+        });   
+        req.on('end', function () {
+        var POST = qs.parse(body); 
+		Item.findOne({_id : POST._id,'quantities':{name:POST.color}},{ quantities.quantity:1}).exec(function(err, quantity) { 
+		//Item.findOne({_id : POST._id,'quantities':{name:POST.color}}).where(quantities.quantity).gt(POST.sum).exec(function(err, item) { 
+		if (err) {
+            res.status(404);
+            res.send('item not found!');
+        }
+        else{
+			console.log("quantity: "+quantity);
+			res.send(quantity);
+        }      
+		});
+	});
 	
 }
