acdd73ca860ee9bbd88e6789539066c8fcd8d068
shoshanasi <shenanigans995@gmail.com>
2017-02-16 09:46:28 +0200

    The signout button works properly

--- public/view/admin/controller/signInController.js
+++ public/view/admin/controller/signInController.js
@@ -53,4 +53,29 @@ $scope.signing = function (user) {
     
 }
 
+
+   $scope.signOut = function () {
+     
+    console.log('in sign out');
+    
+    $http.post('/admin/signOut')
+    .success(function (data) {
+     $scope.PostDataResponse = data;
+     console.log( data);
+     if (data.out = true) {
+        
+        window.location.reload();
+        window.location.replace('#/');
+    }
+})
+    
+    .error(function (data, status, header, config) {
+        console.log("Error: "+data);
+        $scope.ResponseDetails = "Data: " + data +
+        "<hr />status: " + status +
+        "<hr />headers: " + header +
+        "<hr />config: " + config;
+    });                      
+}
+
 });
